411cda01dab58db3a757d8f4ce0b3677a2443dac
BWukong <90988500+BWukong@users.noreply.github.com>
2022-03-30 21:55:23 -0400

    Add files via upload

---
+++ happybot.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+import requests
+import json
+
+TOKEN = 'OTI3NzgxNTc4NDk3NzI0NDE2.YdPN7Q.pNIz5yrWR24NAfrjRb7Ds-BTofo'
+
+client = commands.Bot(command_prefix = '.')
+
+sad_words = ["sad", "bitter", "heartbroken", "mournfal", "unhappy", "depressed", "suicide"]
+
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " - " + json_data[0]['a']
+    return(quote)
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    msg = message.content
+
+    if msg.startswith('$happy'):
+        quote = get_quote()
+        await message.channel.send(quote)
+
+    for word in sad_words:
+        if word in msg:
+            await message.channel.send("https://kidshelpphone.ca/")
+        else:
+            pass
+
+client.run(TOKEN)
